0aafbec502b1799971628ff828de0659baac8c59
KyleParkMedium <kyle.park@themedium.io>
2022-10-17 13:14:59 +0900

    code fix

--- start.sh
+++ start.sh
@@ -352,7 +352,7 @@ function setAnchorPeer() {
 
     echo "Decoding config block to JSON and isolating config to ${CORE_PEER_LOCALMSPID}config.json"
     set -x
-    ${BIN_DIR}/configtxlator proto_decode --input ${TEST_NETWORK_HOME}/channel-artifacts/${CHANNEL_NAME}/config_block.pb --type common.Block --output "${TEST_NETWORK_HOME}/channel-artifacts/${CHANNEL_NAME}/${CORE_PEER_LOCALMSPID}.json" 
+    ${BIN_DIR}/configtxlator proto_decode --input "${TEST_NETWORK_HOME}/channel-artifacts/${CHANNEL_NAME}/config_block.pb" --type common.Block --output "${TEST_NETWORK_HOME}/channel-artifacts/${CHANNEL_NAME}/${CORE_PEER_LOCALMSPID}.json" 
     { set +x; } 2>/dev/null
     
     cat "${TEST_NETWORK_HOME}/channel-artifacts/${CHANNEL_NAME}/${CORE_PEER_LOCALMSPID}.json" | jq .data.data[0].payload.data.config >"${TEST_NETWORK_HOME}/channel-artifacts/${CHANNEL_NAME}/${CORE_PEER_LOCALMSPID}config.json"
@@ -378,120 +378,120 @@ function setAnchorPeer() {
     verifyResult $res "Anchor peer update failed"
 }
 
-function deployChaincode() {
-    CHANNEL_NAME=$1
-    CHAINCODE_NAME=$2
-    shift 2
-    pushd ${TEST_NETWORK_HOME}/chaincodes/${CHAINCODE_NAME}/go
-    GO111MODULE=on go mod vendor
-    popd
-    echo "Finished vendoring Go dependencies"
+# function deployChaincode() {
+#     CHANNEL_NAME=$1
+#     CHAINCODE_NAME=$2
+#     shift 2
+#     pushd ${TEST_NETWORK_HOME}/chaincodes/${CHAINCODE_NAME}/go
+#     GO111MODULE=on go mod vendor
+#     popd
+#     echo "Finished vendoring Go dependencies"
 
-    set -x
-    ${BIN_DIR}/peer lifecycle chaincode package ${TEST_NETWORK_HOME}/packages/${CHAINCODE_NAME}.tar.gz --path ${TEST_NETWORK_HOME}/chaincodes/${CHAINCODE_NAME}/go --lang "golang" --label ${CHAINCODE_NAME}_${CHAINCODE_VERSION} >&${LOG_DIR}/chaincode.log
-    res=$?
-    { set +x; } 2>/dev/null
-    cat ${LOG_DIR}/chaincode.log
-    verifyResult $res "Chaincode packaging has failed"
-    echo "Chaincode is packaged"
+#     set -x
+#     ${BIN_DIR}/peer lifecycle chaincode package ${TEST_NETWORK_HOME}/packages/${CHAINCODE_NAME}.tar.gz --path ${TEST_NETWORK_HOME}/chaincodes/${CHAINCODE_NAME}/go --lang "golang" --label ${CHAINCODE_NAME}_${CHAINCODE_VERSION} >&${LOG_DIR}/chaincode.log
+#     res=$?
+#     { set +x; } 2>/dev/null
+#     cat ${LOG_DIR}/chaincode.log
+#     verifyResult $res "Chaincode packaging has failed"
+#     echo "Chaincode is packaged"
 
-    export CORE_PEER_TLS_ENABLED=true
-    export ORDERER_CA="${TEST_NETWORK_HOME}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
-    export CORE_PEER_LOCALMSPID="Org1MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE="${TEST_NETWORK_HOME}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt"
-    export CORE_PEER_MSPCONFIGPATH="${TEST_NETWORK_HOME}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp"
-    export CORE_PEER_ADDRESS="localhost:7051"
+#     export CORE_PEER_TLS_ENABLED=true
+#     export ORDERER_CA="${TEST_NETWORK_HOME}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+#     export CORE_PEER_LOCALMSPID="Org1MSP"
+#     export CORE_PEER_TLS_ROOTCERT_FILE="${TEST_NETWORK_HOME}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt"
+#     export CORE_PEER_MSPCONFIGPATH="${TEST_NETWORK_HOME}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp"
+#     export CORE_PEER_ADDRESS="localhost:7051"
     
-    set -x
-    ${BIN_DIR}/peer lifecycle chaincode install ${TEST_NETWORK_HOME}/packages/${CHAINCODE_NAME}.tar.gz >&${LOG_DIR}/chaincode.log
-    res=$?
-    { set +x; } 2>/dev/null
-    cat ${LOG_DIR}/chaincode.log
-    verifyResult $res "Chaincode installation on peer0.org${ORG} has failed"
-    echo "Chaincode is installed on peer0.org1"
+#     set -x
+#     ${BIN_DIR}/peer lifecycle chaincode install ${TEST_NETWORK_HOME}/packages/${CHAINCODE_NAME}.tar.gz >&${LOG_DIR}/chaincode.log
+#     res=$?
+#     { set +x; } 2>/dev/null
+#     cat ${LOG_DIR}/chaincode.log
+#     verifyResult $res "Chaincode installation on peer0.org${ORG} has failed"
+#     echo "Chaincode is installed on peer0.org1"
 
-    set -x
-    ${BIN_DIR}/peer lifecycle chaincode queryinstalled >&${LOG_DIR}/chaincode.log
-    res=$?
-    { set +x; } 2>/dev/null
-    cat ${LOG_DIR}/chaincode.log
-    PACKAGE_ID=$(sed -n "/${CHAINCODE_NAME}_${CHAINCODE_VERSION}/{s/^Package ID: //; s/, Label:.*$//; p;}" ${LOG_DIR}/chaincode.log)
-    verifyResult $res "Query installed on peer0.org1 has failed"
-    echo "Query installed successful on peer0.org1 on channel"
+#     set -x
+#     ${BIN_DIR}/peer lifecycle chaincode queryinstalled >&${LOG_DIR}/chaincode.log
+#     res=$?
+#     { set +x; } 2>/dev/null
+#     cat ${LOG_DIR}/chaincode.log
+#     PACKAGE_ID=$(sed -n "/${CHAINCODE_NAME}_${CHAINCODE_VERSION}/{s/^Package ID: //; s/, Label:.*$//; p;}" ${LOG_DIR}/chaincode.log)
+#     verifyResult $res "Query installed on peer0.org1 has failed"
+#     echo "Query installed successful on peer0.org1 on channel"
 
-    set -x
-    ${BIN_DIR}/peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA --channelID ${CHANNEL_NAME} --name ${CHAINCODE_NAME} --version ${CHAINCODE_VERSION} --package-id ${PACKAGE_ID} --sequence ${CHAINCODE_SEQUENCE} ${CHAINCODE_INIT_REQUIRED} ${CHAINCODE_END_POLICY} ${CHAINCODE_COLL_CONFIG} >&${LOG_DIR}/chaincode.log
-    res=$?
-    { set +x; } 2>/dev/null
-    cat ${LOG_DIR}/chaincode.log
-    verifyResult $res "Chaincode definition approved on peer0.org1 on channel '${CHANNEL_NAME}' failed"
-    echo "Chaincode definition approved on peer0.org1 on channel '${CHANNEL_NAME}'"
+#     set -x
+#     ${BIN_DIR}/peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA --channelID ${CHANNEL_NAME} --name ${CHAINCODE_NAME} --version ${CHAINCODE_VERSION} --package-id ${PACKAGE_ID} --sequence ${CHAINCODE_SEQUENCE} ${CHAINCODE_INIT_REQUIRED} ${CHAINCODE_END_POLICY} ${CHAINCODE_COLL_CONFIG} >&${LOG_DIR}/chaincode.log
+#     res=$?
+#     { set +x; } 2>/dev/null
+#     cat ${LOG_DIR}/chaincode.log
+#     verifyResult $res "Chaincode definition approved on peer0.org1 on channel '${CHANNEL_NAME}' failed"
+#     echo "Chaincode definition approved on peer0.org1 on channel '${CHANNEL_NAME}'"
 
-    local rc=1
-    local COUNTER=1
-    # continue to poll
-    # we either get a successful response, or reach MAX RETRY
-    while [ $rc -ne 0 -a $COUNTER -lt $MAX_RETRY ]; do
-        sleep $DELAY
-        echo "Attempting to check the commit readiness of the chaincode definition on peer0.org1, Retry after $DELAY seconds."
-        set -x
-        ${BIN_DIR}/peer lifecycle chaincode checkcommitreadiness --channelID ${CHANNEL_NAME} --name ${CHAINCODE_NAME} --version ${CHAINCODE_VERSION} --sequence ${CHAINCODE_SEQUENCE} ${CHAINCODE_INIT_REQUIRED} ${CHAINCODE_END_POLICY} ${CHAINCODE_COLL_CONFIG} --output json >&${LOG_DIR}/chaincode.log
-        res=$?
-        { set +x; } 2>/dev/null
-        let rc=0
-        for var in "$@"; do
-        grep "$var" ${LOG_DIR}/chaincode.log &>/dev/null || let rc=1
-        done
-        COUNTER=$(expr $COUNTER + 1)
-    done
-    cat ${LOG_DIR}/chaincode.log
-    if test $rc -eq 0; then
-        echo "Checking the commit readiness of the chaincode definition successful on peer0.org on channel '${CHANNEL_NAME}'"
-    else
-        echo "After $MAX_RETRY attempts, Check commit readiness result on peer0.org1 is INVALID!"
-        exit 1
-    fi
+#     local rc=1
+#     local COUNTER=1
+#     # continue to poll
+#     # we either get a successful response, or reach MAX RETRY
+#     while [ $rc -ne 0 -a $COUNTER -lt $MAX_RETRY ]; do
+#         sleep $DELAY
+#         echo "Attempting to check the commit readiness of the chaincode definition on peer0.org1, Retry after $DELAY seconds."
+#         set -x
+#         ${BIN_DIR}/peer lifecycle chaincode checkcommitreadiness --channelID ${CHANNEL_NAME} --name ${CHAINCODE_NAME} --version ${CHAINCODE_VERSION} --sequence ${CHAINCODE_SEQUENCE} ${CHAINCODE_INIT_REQUIRED} ${CHAINCODE_END_POLICY} ${CHAINCODE_COLL_CONFIG} --output json >&${LOG_DIR}/chaincode.log
+#         res=$?
+#         { set +x; } 2>/dev/null
+#         let rc=0
+#         for var in "$@"; do
+#         grep "$var" ${LOG_DIR}/chaincode.log &>/dev/null || let rc=1
+#         done
+#         COUNTER=$(expr $COUNTER + 1)
+#     done
+#     cat ${LOG_DIR}/chaincode.log
+#     if test $rc -eq 0; then
+#         echo "Checking the commit readiness of the chaincode definition successful on peer0.org on channel '${CHANNEL_NAME}'"
+#     else
+#         echo "After $MAX_RETRY attempts, Check commit readiness result on peer0.org1 is INVALID!"
+#         exit 1
+#     fi
 
-    PEER_CONN_PARMS="${PEER_CONN_PARMS} --peerAddresses ${CORE_PEER_ADDRESS}"
-    TLSINFO=$(eval echo "--tlsRootCertFiles \$CORE_PEER_TLS_ROOTCERT_FILE")
-    PEER_CONN_PARMS="${PEER_CONN_PARMS} ${TLSINFO}"
-    # while 'peer chaincode' command can get the orderer endpoint from the
-    # peer (if join was successful), let's supply it directly as we know
-    # it using the "-o" option
-    set -x
-    ${BIN_DIR}/peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA --channelID ${CHANNEL_NAME} --name ${CHAINCODE_NAME} $PEER_CONN_PARMS --version ${CHAINCODE_VERSION} --sequence ${CHAINCODE_SEQUENCE} ${CHAINCODE_INIT_REQUIRED} ${CHAINCODE_END_POLICY} ${CHAINCODE_COLL_CONFIG} >&${LOG_DIR}/chaincode.log
-    res=$?
-    { set +x; } 2>/dev/null
-    cat ${LOG_DIR}/chaincode.log
-    verifyResult $res "Chaincode definition commit failed on peer0.org1 on channel '${CHANNEL_NAME}' failed"
-    echo "Chaincode definition committed on channel '${CHANNEL_NAME}'"
+#     PEER_CONN_PARMS="${PEER_CONN_PARMS} --peerAddresses ${CORE_PEER_ADDRESS}"
+#     TLSINFO=$(eval echo "--tlsRootCertFiles \$CORE_PEER_TLS_ROOTCERT_FILE")
+#     PEER_CONN_PARMS="${PEER_CONN_PARMS} ${TLSINFO}"
+#     # while 'peer chaincode' command can get the orderer endpoint from the
+#     # peer (if join was successful), let's supply it directly as we know
+#     # it using the "-o" option
+#     set -x
+#     ${BIN_DIR}/peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA --channelID ${CHANNEL_NAME} --name ${CHAINCODE_NAME} $PEER_CONN_PARMS --version ${CHAINCODE_VERSION} --sequence ${CHAINCODE_SEQUENCE} ${CHAINCODE_INIT_REQUIRED} ${CHAINCODE_END_POLICY} ${CHAINCODE_COLL_CONFIG} >&${LOG_DIR}/chaincode.log
+#     res=$?
+#     { set +x; } 2>/dev/null
+#     cat ${LOG_DIR}/chaincode.log
+#     verifyResult $res "Chaincode definition commit failed on peer0.org1 on channel '${CHANNEL_NAME}' failed"
+#     echo "Chaincode definition committed on channel '${CHANNEL_NAME}'"
 
-    EXPECTED_RESULT="Version: ${CHAINCODE_VERSION}, Sequence: ${CHAINCODE_SEQUENCE}, Endorsement Plugin: escc, Validation Plugin: vscc"
-    echo "Querying chaincode definition on peer0.org$ on channel '${CHANNEL_NAME}'..."
-    local rc=1
-    local COUNTER=1
-    # continue to poll
-    # we either get a successful response, or reach MAX RETRY
-    while [ $rc -ne 0 -a $COUNTER -lt $MAX_RETRY ]; do
-        sleep $DELAY
-        echo "Attempting to Query committed status on peer0.org1, Retry after $DELAY seconds."
-        set -x
-        ${BIN_DIR}/peer lifecycle chaincode querycommitted --channelID ${CHANNEL_NAME} --name ${CHAINCODE_NAME} >&${LOG_DIR}/chaincode.log
-        res=$?
-        { set +x; } 2>/dev/null
-        test $res -eq 0 && VALUE=$(cat ${LOG_DIR}/chaincode.log | grep -o '^Version: '$CHAINCODE_VERSION', Sequence: [0-9]*, Endorsement Plugin: escc, Validation Plugin: vscc')
-        test "$VALUE" = "$EXPECTED_RESULT" && let rc=0
-        COUNTER=$(expr $COUNTER + 1)
-    done
-    cat ${LOG_DIR}/chaincode.log
-    if test $rc -eq 0; then
-        echo "Query chaincode definition successful on peer0.org1 on channel '${CHANNEL_NAME}'"
-    else
-        echo "After $MAX_RETRY attempts, Query chaincode definition result on peer0.org1 is INVALID!"
-        exit 1
-    fi
+#     EXPECTED_RESULT="Version: ${CHAINCODE_VERSION}, Sequence: ${CHAINCODE_SEQUENCE}, Endorsement Plugin: escc, Validation Plugin: vscc"
+#     echo "Querying chaincode definition on peer0.org$ on channel '${CHANNEL_NAME}'..."
+#     local rc=1
+#     local COUNTER=1
+#     # continue to poll
+#     # we either get a successful response, or reach MAX RETRY
+#     while [ $rc -ne 0 -a $COUNTER -lt $MAX_RETRY ]; do
+#         sleep $DELAY
+#         echo "Attempting to Query committed status on peer0.org1, Retry after $DELAY seconds."
+#         set -x
+#         ${BIN_DIR}/peer lifecycle chaincode querycommitted --channelID ${CHANNEL_NAME} --name ${CHAINCODE_NAME} >&${LOG_DIR}/chaincode.log
+#         res=$?
+#         { set +x; } 2>/dev/null
+#         test $res -eq 0 && VALUE=$(cat ${LOG_DIR}/chaincode.log | grep -o '^Version: '$CHAINCODE_VERSION', Sequence: [0-9]*, Endorsement Plugin: escc, Validation Plugin: vscc')
+#         test "$VALUE" = "$EXPECTED_RESULT" && let rc=0
+#         COUNTER=$(expr $COUNTER + 1)
+#     done
+#     cat ${LOG_DIR}/chaincode.log
+#     if test $rc -eq 0; then
+#         echo "Query chaincode definition successful on peer0.org1 on channel '${CHANNEL_NAME}'"
+#     else
+#         echo "After $MAX_RETRY attempts, Query chaincode definition result on peer0.org1 is INVALID!"
+#         exit 1
+#     fi
 
-}
+# }
 
 function main() {
     startCA
@@ -527,20 +527,20 @@ function main() {
     deployChaincode mychannel0 token-erc-20 "\"Org1MSP\": true"
     echo "finished to deploy chaincode"
     sleep 5
-    createChannelTx mychannel1
-    echo "finished to create channel tx"
-    sleep 1
-    createChannel mychannel1
-    echo "finished to create channel"
-    sleep 1
-    joinChannel mychannel1
-    echo "finished to join channel org1"
-    sleep 1
-    setAnchorPeer mychannel1
-    echo "finished to set anchor peer"
-    sleep 1
-    deployChaincode mychannel1 coin-erc-20 "\"Org1MSP\": true"
-    echo "finished to deploy chaincode"
+    # createChannelTx mychannel1
+    # echo "finished to create channel tx"
+    # sleep 1
+    # createChannel mychannel1
+    # echo "finished to create channel"
+    # sleep 1
+    # joinChannel mychannel1
+    # echo "finished to join channel org1"
+    # sleep 1
+    # setAnchorPeer mychannel1
+    # echo "finished to set anchor peer"
+    # sleep 1
+    # deployChaincode mychannel1 coin-erc-20 "\"Org1MSP\": true"
+    # echo "finished to deploy chaincode"
 }
 
 function verifyResult() {
